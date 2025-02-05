8f95bc16d116737bc881cad2fa7c5040278c536f
Richard Hounslow <rhounslow@gmail.com>
2025-02-05 13:56:25 +1100

    Fix changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import MainContent from "@/components/MainContent";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <MainContent />
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        Test
+      </div>
     </main>
   );
 }
