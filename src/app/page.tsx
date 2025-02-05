2fa53691322566cbc6a81d9cd76d4b5af115bf66
Richard Hounslow <rhounslow@gmail.com>
2025-02-05 13:48:37 +1100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import MainContent from "@/components/MainContent";
+
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <MainContent />
+    </main>
+  );
+}
---
+++ src/components/MainContent.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function MainContent() {
+  return (
+    <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      Test
+    </div>
+  );
+}
